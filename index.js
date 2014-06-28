7379343090b269a41669af2ab589cbff59870225
Jan Krems <jan.krems@gmail.com>
2014-06-27 22:28:05 -0700

    Funny reloader stuff

--- index.js
+++ index.js
@@ -8,7 +8,38 @@ require('node-jsx').install({
 var route = require('quinn-router').route;
 var firstHandler = require('quinn').firstHandler;
 
-module.exports = firstHandler(
-  route(require('./conf/routes')),
-  route(require('./mods/reddit/routes'))
+var runRequestHandler = require('quinn').runRequestHandler;
+function use(outer) {
+  return function (inner) {
+    function normalizedInner(req, params) {
+      return runRequestHandler(inner, req, params);
+    }
+
+    return function(req, params) {
+      return outer(req, params, normalizedInner);
+    }
+  }
+}
+
+function requestLogger(req, params, inner) {
+  console.log('[REQ] %s %s', req.method, req.url);
+  var resP = inner(req, params);
+
+  resP.then(
+    function(res) {
+      console.log('[%s] %s %s', res.statusCode, req.method, req.url);
+    },
+    function(err) {
+      console.log('[ERR] %s %s\n  %s', req.method, req.url, err.message);
+    }
+  );
+
+  return resP;
+};
+
+module.exports = use(requestLogger)(
+  firstHandler(
+    route(require('./conf/routes')),
+    route(require('./mods/reddit/routes'))
+  )
 );
